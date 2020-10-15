788b5ef1a6a7c067c9918b39a577b9c5d1a35489
Hanna Likhuta <Hanna.Likhuta@itechart-group.com>
2020-10-15 11:50:35 +0300

    init project, add basic functionality, delete unnecessary files

---
+++ src/components/app-header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const AppHeader = () => {
+    return (
+      <h1>My Todo list</h1>
+  
+    )
+  };
+
+  export default AppHeader;
---
+++ src/components/todo-list-item.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const TodoListItem = () => {
+    return (
+      <span>My Todo item</span>
+  
+    )
+  };
+
+  export default TodoListItem;
