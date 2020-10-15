68348e23e85b256e4afb9b96e58846e5a7f8387a
Hanna Likhuta <Hanna.Likhuta@itechart-group.com>
2020-10-15 19:04:04 +0300

    clear input after adding new item

--- src/components/add-item/add-item.js
+++ src/components/add-item/add-item.js
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import "./add-item.css";
 
-const AddItem = ({ onAddItem }) => {
-  return (
-    <div className="add-item">
-      <button
-        className="btn btn-outline-secondary"
-        onClick={() => onAddItem('Hello')}
-        type="button"
-      >
-        Add
-      </button>
-    </div>
-  );
-};
+class AddItem extends Component {
+  
+  state = {
+    label: '',
+  }
+
+  onLabelChange = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      label: e.target.value
+    })
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onAddItem(this.state.label);
+    this.setState({
+      label: ''
+    })
+  }
+
+  render() {
+    return (
+      <form className="add-item d-flex" noValidate onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          className="form-control input-text"
+          onChange={this.onLabelChange}
+          placeholder="Whats"
+          value={this.state.label}
+        />
+        <button
+          className="btn btn-outline-secondary"
+          type="submit">
+          Add
+        </button>
+      </form>
+    );
+  }
+}
 
 export default AddItem;
