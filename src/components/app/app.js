bf6db0eb8d4cf0de3adfe5e3527808eaabe3a6ed
Hanna Likhuta <Hanna.Likhuta@itechart-group.com>
2020-10-15 16:32:03 +0300

    refactoring code for app.js

---
+++ src/components/app/app.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import AppHeader from "../app-header";
+import SearchPanel from "../search-panel";
+import TodoList from "../todo-list";
+import ItemStatusFilter from "../item-status-filter";
+
+import "./app.css";
+
+export default class App extends Component {
+  render() {
+    const todoData = [
+      { label: "Drink coffe", important: false, id: 1 },
+      { label: "Builad App", important: true, id: 2 },
+      { label: "Have a lunch", important: false, id: 3 },
+    ];
+    return (
+      <div className="todo-app">
+        <AppHeader todo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+        <TodoList
+          todos={todoData}
+          onDeleted={(id) => console.log("delete ", id)}
+        />
+      </div>
+    );
+  }
+}
+
+
