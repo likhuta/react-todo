c98d4e4523860678410819441351c060398fbd96
Hanna Likhuta <Hanna.Likhuta@itechart-group.com>
2020-10-16 12:07:06 +0300

    init debounce function

---
+++ src/core/debounce.js
@@ -0,0 +1,9 @@
+export const debounce = (fn, delay) => {
+  let timer = null;
+  return function (...args) {
+    timer && clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
+  };
+};
